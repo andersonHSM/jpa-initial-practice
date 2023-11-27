0142451c2f03c82ba7fc7aad8edd3be5c0190f9e
Anderson Menezes <andersonmenezes.dev@gmail.com>
2023-11-27 17:50:47 -0300

    JPA/Hibernate initializing

--- jpa-initial-practice/src/main/java/com/andersonhsm/Main.java
+++ jpa-initial-practice/src/main/java/com/andersonhsm/Main.java
@@ -1,7 +1,15 @@
 package com.andersonhsm;
 
+import domain.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person p1 = new Person(1, "Carlos da Silva", "carlos@gmail.com");
+        Person p2 = new Person(2, "Joaquina Torres", "joaquina@gmail.com");
+        Person p3 = new Person(3, "Ana Maria", "ana@gmail.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
